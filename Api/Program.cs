f77663cabe784c992bc02ba368dc44345fffac98
Mickael Salvi <mickaelsalvi39@gmail.com>
2025-07-20 07:54:23 +0200

    adding yml file

--- Api/Program.cs
+++ Api/Program.cs
@@ -19,7 +19,6 @@ builder.Services.AddScoped<ITourGuideService, TourGuideService>();
 builder.Services.AddScoped<IGpsUtil, GpsUtilWrapper>();
 builder.Services.AddScoped<IRewardCentral, RewardCentralWrapper>();
 
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
